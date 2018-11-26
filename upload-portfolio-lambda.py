1893cadac15697ef57b47f575c41f1d5347f6dfa
mnahm5 <ahmed.nadim59@gmail.com>
2018-11-26 11:34:43 +1100

    Done with upload script

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -18,8 +18,6 @@ build_bucket.download_fileobj('aws-portfolio-project', portfolio_zip)
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
         obj = myzip.open(nm)
-        print nm
-        print mimetypes.guess_type(nm)[0]
         portfolio_bucket.upload_fileobj(obj, nm,
                                         ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
