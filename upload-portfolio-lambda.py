69b27cbb6af867df96203b8c95d93e9d25ef87b1
mnahm5 <ahmed.nadim59@gmail.com>
2018-11-26 14:03:08 +1100

    Automatic update using codepipeline

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -7,7 +7,10 @@ import mimetypes
 
 def lambda_handler(event, context):
     # TODO implement
-    codebuild_bucket_name = 'codebuild.mdnadimahmed.com'
+    location = {
+        "bucketName": 'codebuild.mdnadimahmed.com',
+        "objectKey": 'aws-portfolio-project'
+    }
     portfolio_bucket_name = 'mdnadimahmed.com'
     sns_topic_name = 'arn:aws:sns:ap-southeast-2:947526780140:aws-portfolio-project-update'
 
@@ -15,14 +18,21 @@ def lambda_handler(event, context):
     topic = sns.Topic(sns_topic_name)
 
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                location = artifact["location"]["s3Location"]
+
+        print "Building from" + str(location)
 
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket(portfolio_bucket_name)
-        build_bucket = s3.Bucket(codebuild_bucket_name)
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('aws-portfolio-project', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -31,10 +41,17 @@ def lambda_handler(event, context):
                                                 ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
+        print "Job Done"
+
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
+        print "CodePipeline ID Sent"
+
         topic.publish(Subject="Portfolio Updated", Message="Success")
     except:
         topic.publish(Subject="Portfolio Update Failed", Message="Failed")
-        raise 
+        raise
 
     return {
         'statusCode': 200,
